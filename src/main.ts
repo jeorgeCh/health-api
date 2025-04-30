afe6a3b60b9f6a1ce192a35fc710c282c39c222d
Jorge <jorgechicaiza2512@gmail.com>
2025-04-29 21:33:18 -0500

    connected supabase

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ import {ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  await app.listen(process.env.PORT ?? 3000);
-
   app.enableCors();
-
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
